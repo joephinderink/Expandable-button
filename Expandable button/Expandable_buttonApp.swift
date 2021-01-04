8aa12207284f3a0501e79ab99f77dbaf5b66587f
Joep Hinderink <hello@joephinderink.nl>
2021-01-04 08:43:31 +0100

    Initial Commit

---
+++ Expandable button/Expandable_buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expandable_buttonApp.swift
+//  Expandable button
+//
+//  Created by Joep Hinderink on 04/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Expandable_buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
